991904e7cd12bea39159a6a82640eea1252648d8
aedds <aedds@relias.com>
2020-03-23 17:01:09 -0400

    add bootstrap and rename app to authorquiz

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function AuthorQuiz() {
+  return (
+    <div>Author Quiz</div>
+  );
+}
+
+export default AuthorQuiz;
